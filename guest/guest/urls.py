3709a004c3644e9ac178181bfb83b47aea07852b
zhangman3187 <zhangman3187@126.com>
2017-03-16 19:03:55 +0800

    2017/3/16

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from sign import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/$', views.index),
+    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
+
 ]
